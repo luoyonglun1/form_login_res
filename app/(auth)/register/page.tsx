fc14087ecbabf1d4b8e1725e20a631eac8732f5f
luoyonglun1 <vengroth3322@gmail.com>
2026-01-17 20:04:34 +0700

    push

---
+++ app/(auth)/register/page.tsx
@@ -0,0 +1,9 @@
+import RegisterForm from "@/app/components/registerForm"
+export default function RegisterPage(){
+    return(
+        <div className="w-full flex justify-center h-[100vh] items-center">
+              <RegisterForm/> 
+        </div>
+        
+    )
+}
